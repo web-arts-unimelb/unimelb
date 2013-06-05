483a832c07360bd6b7c8c91e70444ee35467a01d
web.arts.unimelb@gmail.com <web.arts.unimelb@gmail.com>
2013-06-05 10:39:00 +1000

    try to use jquery to fix background issue

--- js/local.js
+++ js/local.js
@@ -25,9 +25,9 @@
           $.backstretch(bgPath, {
             speed: 0
           });
-          
-          //_backstretch_fix();
-          //_backstretch_fix_1();
+         
+					// Fix transparent background issue
+					$('#backstretch-wrapper').css('opacity','.9'); 
         }
         else {
           var bgPath = Drupal.settings.unimelb.background;
@@ -53,84 +53,3 @@
 })(jQuery);
 
 
-function _backstretch_fix()
-{
-	// Quick fix: backstretch missing
-	var default_adjust = 30;
-  var slider_height = $('#slider').height();
-  var main_content_height = $('#main-content').height();
-  
-  var sidebar_height = $('.sidebar-right').height();
-  var the_nav_height = $('.the_nav').height();
-  
-  if(slider_height > 0)
-  {
-  	// On home page
-  	if(sidebar_height > 0)
-  	{
-  		if(main_content_height >= sidebar_height)
-			{
-				back_height = slider_height + main_content_height + default_adjust;
-			}
-			else
-			{
-				back_height = slider_height + sidebar_height + 60;
-			}
-  	}
-  	else
-  	{
-  		back_height = slider_height + main_content_height + default_adjust;
-  	}
-  }
-  else
-  {
-  	// Not on home page
-  	if(sidebar_height > 0)
-  	{
-  		if(main_content_height >= sidebar_height)
-			{
-				back_height = main_content_height + default_adjust;
-			}
-			else
-			{
-				back_height = sidebar_height + 40;
-			}
-  	}
-  	else
-  	{
-  		// Doesn't have sidebar, but navigation menu
-  		if(the_nav_height > 0)
-  		{
-  			if(main_content_height >= the_nav_height)
-  			{
-  				back_height = main_content_height + default_adjust;
-  			}
-  			else
-  			{
-  				back_height = the_nav_height + default_adjust;
-  			}
-  		}
-  		else
-  		{
-  			back_height = main_content_height + default_adjust;
-  		}
-  	}
-  }
-  
-  var back_height_px = back_height + 'px';
-  $('#backstretch-wrapper').css('height', back_height_px);
-}
-
-function _backstretch_fix_1()
-{
-	var g_header_height = $('#g-header').height();
-	var header_height = $('.header').height();
-	var footer_height = $('.footer').height();
-	var footernav_height = $('#footernav').height();
-	var document_height = $(document).height();
-	
-	var adjust = 180;
-	var back_height = document_height - g_header_height - header_height - footer_height - footernav_height - adjust;
-	var back_height_px = back_height + 'px';
-  $('#backstretch-wrapper').css('height', back_height_px);
-}
