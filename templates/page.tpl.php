0b4f4fbe67fd0a5b9d1f7fe6e0cf161e882e92d3
arts-webmaster <web.arts.unimelb@gmail.com>
2013-06-06 16:58:23 +1000

    add missing dropdown-menu to page.tpl.php

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -122,7 +122,19 @@
     </div>
   <?php endif; ?>
 
-  <?php if (!empty($page['feature_menu']) || !empty($site_search_box)): ?>
+	<?php if($dropdown_and_search == TRUE): ?>
+		<div class="arts-dropdown">
+			<div id="arts-dropdown-menu">
+				<?php print render($page['feature_menu']); ?>
+			</div>
+
+			<?php if(!empty($site_search_box)): ?>
+				<div id="arts-dropdown-site-search">
+					<?php print render($site_search_box); ?>
+				</div>
+			<?php endif; ?>
+		</div>
+  <?php elseif (!empty($page['feature_menu']) || !empty($site_search_box)): ?>
     <div class="feature col-8">
       <div id="feature-menu" class="col-6 first">
         <?php print render($page['feature_menu']); ?>
